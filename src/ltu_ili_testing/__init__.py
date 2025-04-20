25912669616bfc51eb0f12599a3aea82e2003f1c
tharvey303 <tharvey303@gmail.com>
2025-04-20 18:38:03 +0100

    initial commit with some basic functionality using synthesizer.
    Can generate SEDs drawn from prior distributions combined from multiple grids.
    
    
    Former-commit-id: 5d4ca997cfaa609e77cd064cd5d1ba48862e89da

---
+++ src/ltu_ili_testing/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from .grid import generate_sfh_grid, generate_metallicity_distribution, generate_emission_models, generate_sfh_basis, sed_grid_generator
+
+
+__all__ = [
+    "generate_sfh_grid",
+    "generate_metallicity_distribution",
+    "generate_emission_models",
+    "generate_sfh_basis",
+    "sed_grid_generator",
+]
