86917d016576cf377874965ee46ebc4554eb96c2
tharvey303 <tharvey303@gmail.com>
2025-04-24 02:14:54 +0100

    new classes for grid generation

--- src/ltu_ili_testing/__init__.py
+++ src/ltu_ili_testing/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .grid import generate_sfh_grid, generate_metallicity_distribution, generate_emission_models, generate_sfh_basis, sed_grid_generator
+from .grid import generate_sfh_grid, generate_metallicity_distribution, generate_emission_models, generate_sfh_basis, sed_grid_generator, GalaxyBasis, generate_constant_R, CombinedBasis
 
 
 __all__ = [
@@ -9,4 +9,7 @@ __all__ = [
     "generate_emission_models",
     "generate_sfh_basis",
     "sed_grid_generator",
+    "GalaxyBasis",
+    "generate_constant_R",
+    "CombinedBasis",
 ]
