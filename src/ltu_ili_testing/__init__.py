31a684e2fc4913f90ee1c6995491d7e5cb8fcfc3
tharvey303 <tharvey303@gmail.com>
2025-04-29 09:32:52 +0100

    Update pyproject, added LatinHypercube sampling
    Former-commit-id: 6636d53c1cf5a3f321992ddf76c4ec82b53c9a7c

--- src/ltu_ili_testing/__init__.py
+++ src/ltu_ili_testing/__init__.py
@@ -2,11 +2,14 @@
 
 from .grid import (
     generate_sfh_grid, generate_metallicity_distribution, generate_emission_models, 
-    generate_sfh_basis, sed_grid_generator, GalaxyBasis, generate_constant_R, CombinedBasis,
-    calculate_muv
+    generate_sfh_basis, GalaxyBasis, generate_constant_R, CombinedBasis,
+    calculate_muv, draw_from_hypercube
 )
 
-from .sbi import SBI_Fitter
+try:
+    from .sbi import SBI_Fitter
+except ImportError:
+    print('Depdencies for SBI not installed. Only the grid generation functions will be available.')
 
 
 __all__ = [
@@ -14,9 +17,9 @@ __all__ = [
     "generate_metallicity_distribution",
     "generate_emission_models",
     "generate_sfh_basis",
-    "sed_grid_generator",
     "GalaxyBasis",
     "generate_constant_R",
+    "draw_from_hypercube",
     "CombinedBasis",
     "calculate_muv",
     "SBI_Fitter",
