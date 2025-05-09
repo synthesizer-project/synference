1. Bug fix for mass scaling 2. added option to set noise scatter for training photometry 3. Added plotting to GalaxyBasis for testing 4. Fixed reloading trained model from pkl.
Former-commit-id: 749b79db1950bb6cd895fe0603623f3d70b1b15a

src/ltu_ili_testing/grid.py
@@ -693,6 +693,8 @@ class GalaxyBasis:
         self.stellar_masses = stellar_masses
         self.redshift_dependent_sfh = redshift_dependent_sfh
         self.params_to_ignore = params_to_ignore
+        self.build_grid = build_grid
+
         self.galaxies = []
 
         if isinstance(self.metal_dists, ZDist.Common):
@@ -912,7 +914,7 @@ class GalaxyBasis:
             **galaxy_kwargs, # most parameters want to be on the emitter
         )
 
-        # Define the number of stellar particles we want
+        # Define the number of stellar particles
         n = stellar_mass.size if isinstance(stellar_mass, unyt_array) else 1
         
 
@@ -1124,6 +1126,214 @@ class GalaxyBasis:
 
         return pipeline
 
+    def plot_random_galaxy(self, masses, **kwargs):
+        """
+        Plot a random galaxy from the list of galaxies.
+        """
+
+        if not self.build_grid:
+            idx = np.random.randint(0, len(self.redshifts))
+            mass = masses[idx]
+            return self.plot_galaxy(idx, stellar_mass=mass*Msun, **kwargs)
+
+    def plot_galaxy(self,
+                    idx,
+                    save: bool = True,
+                    stellar_mass: unyt_quantity = 1e9*Msun,
+                    emission_model_keys: List[str] = ['total'],
+                    out_dir='./'
+                    ):
+        """
+        Plot the galaxy with the given index.
+        """
+        if not self.build_grid and len(self.galaxies) == 0:
+            galaxy_params = {}
+            for param in self.galaxy_params.keys():
+                if isinstance(self.galaxy_params[param], dict):
+                    galaxy_params[param] = self.process_priors(self.galaxy_params[param])
+                elif isinstance(self.galaxy_params[param], (list, np.ndarray)):
+                    galaxy_params[param] = self.galaxy_params[param][idx]
+                else:
+                    galaxy_params[param] = self.galaxy_params[param]
+
+            # Get idx's from requirements and build galaxy directly
+            galaxy = self.create_galaxy(sfh=self.sfhs[idx],
+                                        redshift=self.redshifts[idx],
+                                        metal_dist=self.metal_dists[idx],
+                                        base_mass=stellar_mass,
+                                        **galaxy_params)
+        else:
+            if idx >= len(self.galaxies):
+                raise ValueError(f"Index {idx} out of range for galaxies.")
+
+            galaxy = self.galaxies[idx]
+
+        # Generate spectra
+
+        galaxy.stars.get_spectra(self.emission_model)
+        galaxy.get_observed_spectra(cosmo=self.cosmo, igm=Inoue14)
+
+        fig, ax = plt.subplots(1, 2, figsize=(10, 5), layout='constrained') 
+        
+        plot_dict = {key: galaxy.stars.spectra[key] for key in emission_model_keys}
+        
+        colors = {key: plt.cm.viridis(i/len(emission_model_keys)) for i, key in enumerate(plot_dict.keys())}
+        
+        plot_spectra(
+            plot_dict,
+            show=False,
+            fig=fig,
+            ax = ax[0],
+            x_units=um,
+            quantity_to_plot='fnu',
+            draw_legend=False,
+        )
+
+        # change color of line with label key to the color of the key
+
+        for line in ax[0].lines:
+            label = line.get_label()
+            test_colors = [i.title() for i in colors.keys()]
+            if label in test_colors:
+                pos = test_colors.index(label)
+                label = list(colors.keys())[pos]
+                line.set_color(colors[label])
+                line.set_linewidth(1.5)
+                line.set_label(label)
+
+        ax[0].set_yscale('log')
+
+        def custom_xticks(x, pos):
+            if x == 0:
+                return '0'
+            else:
+                return f'{x/1e4:.1f}'
+
+        ax[0].xaxis.set_major_formatter(FuncFormatter(custom_xticks))
+
+        min_x, max_x = 1e10 * um, 0 * um
+        min_y, max_y = 1e10 * nJy, 0 * nJy
+
+        text_gal = {}
+        for emission_model in emission_model_keys:
+
+            sed = galaxy.stars.spectra[emission_model]
+
+            # Plot photometry
+            phot = sed.get_photo_fnu(filters=self.instrument.filters)
+
+            min_x = min(min_x, np.nanmin(phot.filters.pivot_lams))
+            max_x = max(max_x, np.nanmax(phot.filters.pivot_lams))
+            min_y = min(min_y, np.nanmin(phot.photo_fnu))
+            max_y = max(max_y, np.nanmax(phot.photo_fnu))
+
+            ax[0].plot(phot.filters.pivot_lams, phot.photo_fnu, '+', color=colors[emission_model], path_effects=[PathEffects.withStroke(linewidth=4, foreground='white')])
+
+            # Get the redshift
+            redshift = galaxy.redshift
+
+            # Get the SFH
+            stars_sfh = galaxy.stars.get_sfh()
+            stars_sfh = stars_sfh / np.diff(10**(self.grid.log10age), prepend=0) / yr
+            t, sfh = galaxy.stars.sf_hist_func.calculate_sfh()
+
+            ax[1].plot(10**(self.grid.log10age-6), stars_sfh, label=f'{emission_model} SFH', color=colors[emission_model])
+            ax[1].plot(t/1e6, sfh/np.max(sfh) * np.max(stars_sfh), label=f'Requested {emission_model} SFH', color=colors[emission_model], linestyle='--')
+
+            mass = galaxy.stars.initial_mass
+            if mass == 0:
+                text_gal[emission_model] = f'{emission_model}     \nNo stars'
+            else:
+                age = galaxy.stars.calculate_mean_age()
+                zmet = galaxy.stars.calculate_mean_metallicity()
+
+                text_gal[emission_model] = f'{emission_model}     \nAge {age.to(Myr):.0f}\n$\log_{{10}}$Z: {np.log10(zmet.value):.2f}\nM$_\star$: {np.log10(mass):.1f} M$_\odot$'
+
+        ax[0].legend(loc='upper right', fontsize=6, ncols=3)
+        # Set the x-axis limits
+        if max_y > 1 * nJy:
+            min_y = max(min_y, 1 * nJy)
+        ax[0].set_xlim(0.9*min_x, 1.1*max_x)
+        ax[0].set_ylim(0.9*min_y, 2*max_y)       
+
+        textstr = '\n'.join((
+            r'$z = %.2f$' % (redshift),
+        ))
+
+        textstr += '\n' + '\n'.join(text_gal.values())
+
+        # add galaxy params
+
+        textstr += '\n' + '\n'.join([f'{key}: {value:.2f}' for key, value in galaxy_params.items()])
+
+        # these are matplotlib.patch.Patch properties
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        # place a text box in upper left in axes coords
+        ax[1].text(0.95, 0.72, textstr, transform=ax[1].transAxes, fontsize=12, horizontalalignment='right',
+                verticalalignment='top', bbox=props)
+
+        # add a secondary axis with AB magnitudes
+
+        # 31.4
+
+        def ab_to_jy(f):
+            return 1e9 * 10**(f/(-2.5) -8.9)  
+
+        def jy_to_ab(f):
+            f = f/1e9
+            return -2.5 * np.log10(f) + 8.9
+
+        secax = ax[0].secondary_yaxis('right', functions=(jy_to_ab, ab_to_jy))
+        # set scalar formatter
+
+        max_age = self.cosmo.age(redshift) - self.cosmo.age(20)
+        max_age = max_age.to(u.Myr).value 
+
+        ax[1].set_xlim(0, 1.05*max_age)
+
+        secax.yaxis.set_major_formatter(ScalarFormatter())
+        secax.yaxis.set_minor_formatter(ScalarFormatter())
+        secax.set_ylabel('Flux Density [AB mag]')
+
+        ax[1].set_xlabel('Time [Myr]')
+        ax[1].set_ylabel('SFH [M$_\odot$ yr$^{-1}$]')
+        #ax[1].set_yscale('log')
+        ax[1].legend()
+
+        self.tmp_redshift = redshift    
+        self.tmp_time_unit = u.Myr
+        secax = ax[1].secondary_xaxis("top", functions=(self._time_convert, self._z_convert))
+        secax.set_xlabel("Redshift")
+
+        # Put a vertical line at maximum redshift
+
+        secax.set_xticks([6, 7, 8, 10, 12, 14, 15, 20])
+
+        if save:
+            fig.savefig(f'{out_dir}/{self.model_name}_{idx}.png', dpi=300)
+            plt.close(fig)
+
+        return fig
+
+    def _time_convert(self, lookback_time):
+        time_unit = getattr(self, 'tmp_time_unit', u.yr)
+        lookback_time = lookback_time * time_unit
+        return z_at_value(
+            self.cosmo.lookback_time,
+            self.cosmo.lookback_time(self.tmp_redshift) + lookback_time,
+        ).value
+
+    def _z_convert(self, z):
+        if type(z) in [list, np.ndarray] and len(z) == 0:
+            return np.array([])
+        
+        time_unit = getattr(self, 'tmp_time_unit', u.yr)
+        
+        return (
+            self.cosmo.lookback_time(z) - self.cosmo.lookback_time(self.tmp_redshift)
+        ).to(time_unit).value
+
+
 class CombinedBasis:
     def __init__(self, 
                 bases: List[Type[GalaxyBasis]],
@@ -1133,7 +1343,7 @@ class CombinedBasis:
                 combination_weights: np.ndarray,
                 out_name: str ='combined_basis',
                 out_dir: str ='../output/',
-                base_mass: unyt_array =1e9 * Msun,
+                base_mass: unyt_array = 1e9 * Msun,
                 draw_parameter_combinations: bool = True,
                 ) -> None:
         
@@ -1909,11 +2119,12 @@ class CombinedBasis:
                     raise ValueError(f"No galaxies found for redshift {redshift} in base {base.model_name}. Check your redshift array.")
 
                 # Calculate the scaling factor for each base
-
                 if combination == 1.0:
-                    scaling_factors = [1.0]
+                    scaling_factors = mass_weights / mass
                 else:
                     scaling_factors = mass_weights[i] / mass
+
+                print(f"Scaling factors: {scaling_factors}")
 
                 base_photometry = np.array([pipeline_outputs[base.model_name]['observed_photometry'][filter_code][mask] for filter_code in filter_codes], dtype=np.float32)
 
@@ -1955,6 +2166,7 @@ class CombinedBasis:
 
             # Calculate the total number of combinations
             dimension = np.prod([i.shape[-1] for i in scaled_photometries])
+            print(f"Dimension: {dimension}")
 
             output_array = np.zeros((scaled_photometries[0].shape[0], dimension))
             params_array = np.zeros((len(param_columns), dimension))

src/ltu_ili_testing/sbi.py
@@ -7,6 +7,7 @@ from scipy import stats
 from scipy.interpolate import interp1d
 from scipy.spatial.distance import euclidean
 import torch
+import glob
 import torch.nn as nn
 import torch.nn.functional as F
 import h5py
@@ -14,7 +15,7 @@ import numpy as np
 from unyt import unyt_array, nJy, Msun
 import re
 import operator
-from typing import List, Dict, Union, Iptional, Tuple
+from typing import List, Dict, Union, Optional, Tuple
 import optuna
 import sys
 from io import StringIO
@@ -23,6 +24,7 @@ import matplotlib.pyplot as plt
 from joblib import dump, load
 import json
 from datetime import datetime
+from astropy.visualization import hist
 
 import torch
 import torch.nn as nn
@@ -38,12 +40,12 @@ from sklearn.preprocessing import StandardScaler
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-verbose = False
+verbose = True
 if verbose:
     print('Device:', device)
     print("Pytorch version: " + torch.__version__)
     print("ROCM HIP version: " + torch.version.hip)
-    print("CUDA version: " + torch.version.cuda)
+    #print("CUDA version: " + torch.version.cuda)
 torch.cuda.set_device(f'{device}:0')
 
 # get path of this file
@@ -154,6 +156,7 @@ class SBI_Fitter:
         # See self.update_parameter_array() for more details.
         self.fitted_parameter_array = parameter_array
         self.fitted_parameter_names = parameter_names
+        self.simple_fitted_parameter_names = [i.split('/')[-1] for i in parameter_names]
 
         # Feature array and names
         self.feature_array = feature_array
@@ -195,6 +198,8 @@ class SBI_Fitter:
 
     def update_parameter_array(self,
                                parameters_to_remove: list = [],
+                               delete_rows=[],
+                               n_scatters: int = 1,
                                ) -> None:
         '''
         Removes any parameter in self.provided_feature_parameters from the parameter array and parameter names
@@ -206,11 +211,23 @@ class SBI_Fitter:
                 index = list(self.parameter_names).index(param)
                 self.fitted_parameter_array = np.delete(self.parameter_array, index, axis=1)
                 self.fitted_parameter_names = np.delete(self.parameter_names, index)
+                self.simple_fitted_parameter_names = np.delete(self.simple_fitted_parameter_names, index)
+
+
+        # Remove any rows in delete_rows
+        if len(delete_rows) > 0:
+            self.fitted_parameter_array = np.delete(self.fitted_parameter_array, delete_rows, axis=0)
+
+        # Now duplicate the parameter array n_scatters times. E.g. if it was [[1], [2], [3]] and n_scatters is 3, it should be [[1], [1], [1], [2], [2], [2], [3], [3], [3]]
+        if n_scatters > 1:
+            self.fitted_parameter_array = np.repeat(self.fitted_parameter_array, n_scatters, axis=0)
+
 
     @classmethod
     def init_from_hdf5(cls, 
                        model_name: str,
                        hdf5_path: str,
+                       return_output=False,
                        ):
 
         """
@@ -227,6 +244,9 @@ class SBI_Fitter:
         # Training data if unnormalized and not setup as correct features yet. 
 
         output = load_grid_from_hdf5(hdf5_path)
+
+        if return_output:
+            return output
 
         raw_photometry_grid = output['photometry']
         raw_photometry_names = output['filter_codes']
@@ -310,14 +330,14 @@ class SBI_Fitter:
         Returns:
         --------
         np.ndarray
-            Scattered photometry array of shape (m*N_scatters, n)
+            Scattered photometry array of shape (m, n_scatters*n)
         '''
         # Get input array dimensions
         m, n = photometry_array.shape
         
         # Verify dimensions match
-        if n != len(depths):
-            raise ValueError(f"Mismatch in dimensions: photometry_array has {n} columns but depths has {len(depths)} elements")
+        if m != len(depths):
+            raise ValueError(f"Mismatch in dimensions: photometry_array has {m} columns but depths has {len(depths)} elements")
         
         # Convert depths based on units
         if hasattr(photometry_array, 'units') and photometry_array.units == 'ABmag':
@@ -328,22 +348,19 @@ class SBI_Fitter:
             depths_std = depths.to(self.raw_photometry_units).value / depth_sigma
         
         # Pre-allocate output array with correct dimensions
-        output_arr = np.zeros((m * N_scatters, n))
+        output_arr = np.zeros((m, N_scatters * n))
         
         # Generate all random values at once for better performance
-        # Shape will be (n, m, N_scatters) for broadcasting
-        random_values = np.random.normal(scale=depths_std[:, np.newaxis, np.newaxis], 
-                                        size=(n, m, N_scatters))
-        
-        # Reshape and transpose to get correct dimensions for addition
-        random_values = np.transpose(random_values, (1, 2, 0))  # -> (m, N_scatters, n)
-        
-        # Add the photometry values (broadcasting)
-        scattered_values = photometry_array[:, np.newaxis, :] + random_values  # -> (m, N_scatters, n)
-        
-        # Reshape to final output shape (m*N_scatters, n)
-        output_arr = scattered_values.reshape(m * N_scatters, n)
-        
+        random_values = np.random.normal(loc=0, scale=depths_std[:, None], size=(m, N_scatters * n))
+        # Reshape the random values to match the output array
+        random_values = random_values.reshape(m, N_scatters, n)
+        # Repeat the photometry array for each scatter
+        photometry_repeated = np.repeat(photometry_array[:, None, :], N_scatters, axis=1)
+        # Add the random values to the repeated photometry array
+        output_arr = photometry_repeated + random_values
+        # Reshape the output array to have the correct dimensions
+        output_arr = output_arr.reshape(m, N_scatters * n)
+
         return output_arr
 
     def create_feature_array_from_raw_photometry(self,
@@ -380,7 +397,7 @@ class SBI_Fitter:
             simulate_missing_fluxes:  boolean, default False. CURRENTLY UNIMPLEMENTED.
                 This would allow missing photometry for some fraction of the time, which could be marked
                 with a specific filler flag, or a seperate boolean row/column to flag missing data. 
-             
+            TODO: How should normalization work with the scattering?
 
         Returns:
             The feature array and feature names.
@@ -401,10 +418,10 @@ class SBI_Fitter:
             assert isinstance(depths, unyt_array)
             self.phot_depths = depths
 
-            new_phot_array = self._apply_depths(depths, photometry_array, scatter_fluxes)
-            depth_message = f'Scattering photometry {scatter_fluxes} times for each row'
+            phot = self._apply_depths(depths, phot, scatter_fluxes)
+            print(f'Scattering photometry {scatter_fluxes} times for each row')
 
-        
+        delete_rows = []
         # Normalize the photometry grid
         if normalize_method is not None:
             if normalize_method in self.raw_photometry_names:
@@ -443,7 +460,8 @@ class SBI_Fitter:
                 norm_index = list(self.supplementary_parameter_names).index(normalize_method)
                 norm_unit = self.supplementary_parameter_units[norm_index]
                 normalization_factor = self.supplementary_parameters[norm_index, :]
-
+                # count where normalzation is 0
+               
                 assert normalization_factor.shape[0] == self.raw_photometry_grid.shape[1], "Normalization factor should have the same shape as the photometry grid."
                 assert norm_unit == self.raw_photometry_units, "Normalization factor should have the same units as the photometry grid."
                 
@@ -475,6 +493,7 @@ class SBI_Fitter:
         else:
             normed_photometry = self.raw_photometry_grid
             normalization_factor_converted = np.ones(normed_photometry.shape[1])
+            normalization_factor = normalization_factor_converted
             raw_photometry_names = np.array(self.raw_photometry_names)
 
             # Convert the photometry to the desired units
@@ -483,7 +502,13 @@ class SBI_Fitter:
                 normed_photometry = -2.5 * np.log10(unyt_array(normed_photometry, units=self.raw_photometry_units).to('uJy').value) + 23.9
             else:
                 normed_photometry = unyt_array(normed_photometry, units=self.raw_photometry_units).to(normed_flux_units).value
-    
+
+        if np.sum(normalization_factor == 0.0) > 0:
+            # delete these indexes from the photometry 
+            print(f'Warning: Normalization factor is 0.0 for {np.sum(normalization_factor == 0.0)} rows. These will be deleted.')
+            delete_rows.extend(np.where(normalization_factor == 0.0)[0].tolist())
+
+
         if normed_flux_units == 'AB':
             # very small fluxes can blow up here and go to infinity. Set a maximum difference of some large amount - say 50
             # If not normalized, set minmum flux to 50 AB.
@@ -535,6 +560,10 @@ class SBI_Fitter:
                     # Add the feature name
                     feature_names[len(raw_photometry_names) + i] = feature
 
+
+        # Remove any rows in delete_rows
+        if len(delete_rows) > 0:
+            feature_array = np.delete(feature_array, delete_rows, axis=1)
         
         assert '' not in feature_names, "Feature names should not be empty. Please check the extra features."
 
@@ -554,7 +583,7 @@ class SBI_Fitter:
                 print(f'{feature_name}: {np.min(feature_array[pos]):.3f} - {np.max(feature_array[pos]):.3f} {self.feature_units[pos]}')
             print('---------------------------------------------')
 
-        self.update_parameter_array()
+        self.update_parameter_array(delete_rows=delete_rows)
 
         return feature_array, feature_names
 
@@ -622,6 +651,15 @@ class SBI_Fitter:
         
         low = np.array(low)
         high = np.array(high)
+
+        # Print prior ranges
+        if verbose:
+            print('---------------------------------------------')
+            print('Prior ranges:')
+            print('---------------------------------------------')
+            for i, param in enumerate(self.fitted_parameter_names):
+                print(f'{param}: {low[i]:.2f} - {high[i]:.2f}')
+            print('---------------------------------------------')
 
         low = torch.tensor(low, dtype=torch.float32, device=self.device)
         high = torch.tensor(high, dtype=torch.float32, device=self.device)
@@ -735,7 +773,7 @@ class SBI_Fitter:
         if name_append == 'timestamp':
             name_append = f'_{self._timestamp}'
         
-        if os.path.exists(f'{out_dir}/{self.name}{name_append}_single_sbi_params.pkl') and save_model:
+        if os.path.exists(f'{out_dir}/{self.name}{name_append}_params.pkl') and save_model:
             print('Model with same name already exists. Please change the name of this model or delete the existing one.')
             return None
 
@@ -743,6 +781,13 @@ class SBI_Fitter:
             train_indices, test_indices = self.split_dataset(train_fraction=train_test_fraction, random_seed=random_seed)
 
         # Prepare data
+
+        start_time = datetime.now()
+
+        assert len(train_indices) > 0, "Training indices should not be empty."
+        assert len(test_indices) > 0, "Test indices should not be empty."
+
+        assert self.feature_array.shape[0] == self.fitted_parameter_array.shape[0], "Feature array and parameter array should have the same number of samples."
 
         X_train = self.feature_array[train_indices]
         y_train = self.fitted_parameter_array[train_indices]
@@ -897,11 +942,16 @@ class SBI_Fitter:
             }
             dump(param_dict, f"{out_dir}/{self.name}{name_append}_params.pkl", compress=3)
         
+        end_time = datetime.now()   
+
+        elapsed_time = end_time - start_time
+        print(f'Time to train model(s): {elapsed_time}')
+
         if plot:
             self.plot_diagnostics(X_train=X_scaled, y_train=y_scaled, 
                                   X_test=X_test,
                                   y_test=y_test,
-                                  plots_dir=out_dir)
+                                  plots_dir=f'{out_dir}/plots/')
 
         return posteriors, stats
 
@@ -1079,10 +1129,39 @@ class SBI_Fitter:
         ax.set_ylabel('Log probability')
         ax.legend()
 
-        if not os.path.exists(f'{plots_dir}/{self.name}'):
-            os.makedirs(f'{plots_dir}/{self.name}')
-
         fig.savefig(f'{plots_dir}/loss.png', dpi=300)
+
+    def plot_histogram_parameter_array(self,
+        bins='knuth',
+        plots_dir: str = f'{code_path}/models/name/plots/',
+        seperate_test_train=False,
+        ):
+        '''Plot histogram of each parameter using astropy.visualization.hist'''
+
+        fig, axes = plt.subplots(1, len(self.fitted_parameter_names), figsize=(15, 5))
+        for i, param in enumerate(self.simple_fitted_parameter_names):
+            axes[i].set_title(param)
+            axes[i].set_xlabel('Value')
+            axes[i].set_ylabel('Count')
+            if seperate_test_train:
+                hist(self._y_train[:, i], ax=axes[i], bins=bins, label='Train')
+                hist(self._y_test[:, i], ax=axes[i], bins=bins, label='Test')
+                axes[i].legend()
+            else:
+                hist(self.fitted_parameter_array[:, i], ax=axes[i], bins=bins)
+
+
+        plt.tight_layout()
+
+        plots_dir = plots_dir.replace('name', self.name)
+
+        if not os.path.exists(plots_dir):
+            os.makedirs(plots_dir)
+
+        print('saving', f'{plots_dir}/param_histogram.png')
+        fig.savefig(f'{plots_dir}/param_histogram.png', dpi=300)
+
+        return fig
 
     def plot_posterior(self,
                     ind: int = 'random',
@@ -1118,9 +1197,10 @@ class SBI_Fitter:
         # use ltu-ili's built-in validation metrics to plot the posterior for this point
         metric = PlotSinglePosterior(
             num_samples=num_samples, sample_method=sample_method,
-            labels=self.fitted_parameter_names,
+            labels=self.simple_fitted_parameter_names,
             out_dir=plots_dir,
         )
+       
         fig = metric(
             posterior=self.posteriors,
             x_obs = X[ind], theta_fid=y[ind],
@@ -1226,7 +1306,7 @@ class SBI_Fitter:
 
         metric = PosteriorCoverage(
             num_samples=num_samples, sample_method=sample_method, 
-            labels=self.fitted_parameter_names,
+            labels=self.simple_fitted_parameter_names,
             plot_list = plot_list,
             out_dir=plots_dir,
         )
@@ -1290,8 +1370,18 @@ class SBI_Fitter:
         """
         Load the model from a pickle file.
         Args:
-            model_file: Path to the pickle file.
+            model_file: Path to the pickle file (or folder if only one model stored in the folder).
         """
+
+        if os.path.isdir(model_file):
+            files = glob.glob(os.path.join(model_file, '*_posterior.pkl'))
+            if len(files) == 0:
+                raise ValueError(f"No parameter files found in {model_file}.")
+            elif len(files) > 1:
+                raise ValueError(f"Multiple parameter files found in {model_file}. Please specify a single file.")
+            model_file = files[0]
+
+
         with open(model_file, 'rb') as f:
             posteriors = load(f)
         #
@@ -1310,7 +1400,43 @@ class SBI_Fitter:
         if set_self:
             self.posteriors = posteriors
 
-        return posteriors, stats
+        params = model_file.replace('posterior.pkl', 'params.pkl')
+        if os.path.exists(params):
+            with open(params, 'rb') as f:
+                params = load(f)
+
+            if set_self:
+
+                # Set attributes of class again.
+                self.fitted_parameter_names = params['fitted_parameter_names']
+                self.simple_fitted_parameter_names = [i.split('/')[-1] for i in self.fitted_parameter_names]
+                self.fitted_parameter_array = params['parameter_array']
+                print(params['parameter_array'].shape)
+                self.feature_names = params['feature_names']
+                self.feature_array = params['feature_array']
+                self.parameter_array = params['parameter_array']
+
+                self._train_args = params['train_args']
+                self._prior = params['prior']
+                
+                self._ensemble_model_types = params['ensemble_model_types']
+                self._ensemble_model_args = params['ensemble_model_args']
+                self._train_indices = params['train_indices']
+                self._test_indices = params['test_indices']
+
+                self._train_fraction = params['train_fraction']
+
+                self._X_test = self.feature_array[self._test_indices]
+                self._y_test = self.fitted_parameter_array[self._test_indices]
+                self._X_train = self.feature_array[self._train_indices]
+                self._y_train = self.fitted_parameter_array[self._train_indices]
+
+                
+        else:
+            print(f"Warning: No parameter file found for {model_file}.")
+            params = None
+
+        return posteriors, stats, params
 
     @property
     def _timestamp(self):