a492f11ed96b95f000dc48e7f875df82771ae40a
tharvey303 <tharvey303@gmail.com>
2025-06-04 16:37:21 +0100

    Changes 1. Running grid generation pipeline in batches 2. Added EmpirialNoiseGeneration model 3. Added ability to use empirical noise to simulator and SBI-Fitter 4. New tests for Pop III models
    Former-commit-id: 97484451a80761bd4e99d8de43e4de39bd4bb518

--- src/ltu_ili_testing/__init__.py
+++ src/ltu_ili_testing/__init__.py
@@ -3,7 +3,8 @@
 from .grid import (
     generate_sfh_grid, generate_metallicity_distribution, generate_emission_models, 
     generate_sfh_basis, GalaxyBasis, generate_constant_R, CombinedBasis,
-    calculate_muv, draw_from_hypercube, GalaxySimulator, generate_random_DB_sfh
+    calculate_muv, draw_from_hypercube, GalaxySimulator, generate_random_DB_sfh,
+    EmpiricalUncertaintyModel
 )
 
 try:
@@ -26,5 +27,6 @@ __all__ = [
     "SBI_Fitter",
     "MissingPhotometryHandler",
     "GalaxySimulator",
-    "generate_random_DB_sfh"
+    "generate_random_DB_sfh",
+    "EmpiricalUncertaintyModel",
 ]
