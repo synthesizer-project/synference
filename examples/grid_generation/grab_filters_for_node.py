9385d528fadb96fea68ed3117e846435d9facfc0
Thomas Harvey <tharvey303@gmail.com>
2025-07-01 14:02:50 +0100

    Changes to perform sbi fitting on cosma

--- examples/grid_generation/grab_filters_for_node.py
+++ examples/grid_generation/grab_filters_for_node.py
@@ -3,11 +3,22 @@ from synthesizer.instruments import FilterCollection
 from sbifitter import generate_constant_R
 
 filter_codes = [
+    "HST/ACS_WFC.F435W",
+    "HST/ACS_WFC.F475W",
+    "HST/ACS_WFC.F606W",
     "JWST/NIRCam.F070W",
+    "HST/ACS_WFC.F775W",
+    "HST/ACS_WFC.F814W",
+    "HST/ACS_WFC.F850LP",
     "JWST/NIRCam.F090W",
+    "HST/WFC3_IR.F105W",
+    "HST/WFC3_IR.F110W",
     "JWST/NIRCam.F115W",
+    "HST/WFC3_IR.F125W",
     "JWST/NIRCam.F140M",
+    "HST/WFC3_IR.F140W",
     "JWST/NIRCam.F150W",
+    "HST/WFC3_IR.F160W",
     "JWST/NIRCam.F162M",
     "JWST/NIRCam.F182M",
     "JWST/NIRCam.F200W",
@@ -23,13 +34,14 @@ filter_codes = [
     "JWST/NIRCam.F444W",
     "JWST/NIRCam.F460M",
     "JWST/NIRCam.F480M",
+    "JWST/MIRI.F560W",
+    "JWST/MIRI.F770W",
 ]
-
 # Consistent wavelength grid for both SPS grids and filters
 new_wav = generate_constant_R(R=300)
 
 filterset = FilterCollection(filter_codes, new_lam=new_wav)
 
 filterset.write_filters(
-    "/cosma7/data/dp276/dc-harv3/work/ltu-ili_testing/scripts/filters/JWST.hdf5"
+    "/cosma7/data/dp276/dc-harv3/work/sbifitter/examples/grid_generation/filters/HST+JWST.hdf5"
 )
