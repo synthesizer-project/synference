5a98608efabd357246d383eeded45553d0e63a30
Sophie Newman <sophie.newman@port.ac.uk>
2025-08-22 15:13:43 +0100

    ruff formatter, add comments to basic_model.py

--- examples/sbi/basic_model.py
+++ examples/sbi/basic_model.py
@@ -1,14 +1,14 @@
 import os
 from sbifitter import SBI_Fitter
 
+# Get the paths to your training catalogue and where to save your trained model
 file_path = os.path.dirname(os.path.realpath(__file__))
 grid_folder = os.path.join(os.path.dirname(os.path.dirname(file_path)), "grids")
 output_folder = os.path.join(os.path.dirname(os.path.dirname(file_path)), "models")
 
-grid_path = (
-    f"""{grid_folder}/grid_BPASS_Chab_LogNormal_SFH_z=0.1_logN_4.0_Calzetti_v1.hdf5"""  # noqa: E501
-)
+grid_path = f"{grid_folder}/grid_BPASS_Chab_LogNormal_SFH_z=0.1_logN_4.0_Calzetti_v1.hdf5"  # noqa: E501
 
+# Initialise the fitter
 fitter = SBI_Fitter.init_from_hdf5(
     "basic_model",
     grid_path,
@@ -16,12 +16,12 @@ fitter = SBI_Fitter.init_from_hdf5(
     device="cpu",
 )
 
+# Create a feature array from the raw photometry grid
+fitter.create_feature_array_from_raw_photometry(
+    extra_features=[], normalize_method=None, parameters_to_remove=["redshift"]
+)
 
-fitter.create_feature_array_from_raw_photometry(extra_features=[],
-                                                normalize_method=None,
-                                                parameters_to_remove=['redshift'])
-
-
+# Run SBI with an emsemble network!
 fitter.run_single_sbi(
     n_nets=3,
     backend="lampe",
