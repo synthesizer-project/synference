spectral inference fixes - more options for normalization

examples/grid_generation/scripts/generate_spectral_grid.py
@@ -96,7 +96,8 @@ grid_dir = os.environ["SYNTHESIZER_GRID_DIR"]
 # path for this file
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
-out_dir = "/home/tharvey/work/synference/grids"
+from synference import grid_folder
+out_dir = grid_folder
 
 
 def continuity_agebins(
@@ -174,11 +175,11 @@ except Exception:
     n_proc = 6
 
 av_to_tau_v = 1.086  # conversion factor from Av to tau_v for the dust attenuation curve
-overwrite = False  # whether to overwrite existing grids
-Nmodels = 100_000  # 00  # _000
+overwrite = False  # whether to overwrite existing libraries
+Nmodels = 100  # 00  # _000
 batch_size = 40_000  # number of models to generate in each batch
-redshift = (0.01, 12)
-masses = (4, 12)
+redshift = 0
+masses = 9.0  # log stellar masses
 max_redshift = 20  # gives maximum age of SFH at a given redshift
 cosmo = Planck18  # cosmology to use for age calculations
 
@@ -192,6 +193,7 @@ dust_birth_fraction = (
 )  # multiplier for the attenuation av for the birth cloud
 log_zmet = (-4, -1.39)  # max of grid (e.g. 0.04)
 
+masses = np.full(Nmodels, masses)
 
 sfhs = {
     "log_normal": {
@@ -208,8 +210,6 @@ sfhs = {
 }
 
 full_params_base = {
-    "redshift": redshift,
-    "log_masses": masses,
     "log_Av": logAv,  # Av in magnitudes
     "dust_birth_fraction": dust_birth_fraction,
     "log_zmet": log_zmet,
@@ -222,7 +222,7 @@ for sfh_name, sfh_params in sfhs.items():
 
     sfh_name = str(sfh_type).split(".")[-1].split("'")[0]
 
-    name = f"BPASS_Chab_{sfh_name}_SFH_{redshift[0]}_z_{redshift[1]}_logN_{np.log10(Nmodels):.1f}_CF00_v2"  # noqa: E501
+    name = f"BPASS_Chab_{sfh_name}_SFH_z0_logN_{np.log10(Nmodels):.1f}_CF00_v2"  # noqa: E501
     if os.path.exists(f"{out_dir}/v2/spectral_grid_{name}.hdf5") and not overwrite:
         print(f"Grid {name} already exists, skipping.")
         continue
@@ -262,9 +262,6 @@ for sfh_name, sfh_params in sfhs.items():
     # Metallicity
     Z_dists = [ZDist.DeltaConstant(log10metallicity=log_z) for log_z in all_param_dict["log_zmet"]]
 
-    # Redshifts
-    redshifts = np.array(all_param_dict["redshift"])
-
     if sfh_type == SFH.DenseBasis:
         # Draw SFH params from prior
         Nparam_SFH = sfh_params["Nparam_SFH"]
@@ -292,20 +289,20 @@ for sfh_name, sfh_params in sfhs.items():
         # Add logSFR to all_param_dict
         all_param_dict["log_sfr"] = np.array(logsfrs)
         # UNCOMMENT IN GENERAL! Just because main doesn't have this brnch.
-        elif sfh_type == SFH.Continuity:
-            # Draw from prior.
-            sfh_models = []
-            for i in tqdm(range(Nmodels)):
-                z_i = all_param_dict["redshift"][i]
-                agebins = sfh_params["agebins"](z_i, cosmo=cosmo, Nbins=6)
-                sfh_models.append(
-                    sfh_params["sfh_type"].init_from_prior(
-                        agebins,
-                        df=sfh_params["df"],
-                        scale=sfh_params["scale"],
-                        limits=(-30, 30),
-                    )
-        )
+    elif sfh_type == SFH.Continuity:
+        # Draw from prior.
+        sfh_models = []
+        for i in tqdm(range(Nmodels)):
+            z_i = redshift if isinstance(redshift, (int, float)) else all_param_dict["redshift"][i]
+            agebins = sfh_params["agebins"](z_i, cosmo=cosmo, Nbins=6)
+            sfh_models.append(
+                sfh_params["sfh_type"].init_from_prior(
+                    agebins,
+                    df=sfh_params["df"],
+                    scale=sfh_params["scale"],
+                    limits=(-30, 30),
+                )
+    )
 
     else:
         if "beta" in sfh_param_names:
@@ -315,7 +312,9 @@ for sfh_name, sfh_params in sfhs.items():
             sfh_param_names=sfh_param_names,
             sfh_param_arrays=[all_param_dict[param] for param in sfh_param_names],
             sfh_param_units=sfh_params.get("sfh_units", None),
-            redshifts=redshifts,
+            redshifts=redshift
+                if isinstance(redshift, (int, float))
+                else all_param_dict["redshift"],
             max_redshift=max_redshift,
             cosmo=cosmo,
             iterate_redshifts=False,
@@ -403,7 +402,7 @@ for sfh_name, sfh_params in sfhs.items():
 
     basis = GalaxyBasis(
         model_name=f"sps_{name}",
-        redshifts=redshifts,
+        redshifts=redshift,
         grid=grid,
         emission_model=emission_model,
         sfhs=sfh_models,
@@ -419,10 +418,10 @@ for sfh_name, sfh_params in sfhs.items():
 
     combined_basis = CombinedBasis(
         bases=[basis],
-        log_stellar_masses=all_param_dict["log_masses"],
+        log_stellar_masses=masses,
         base_emission_model_keys=["total"],
         combination_weights=None,
-        redshifts=redshifts,
+        redshifts=redshift,
         out_name=f"grid_{name}",
         out_dir=out_dir,
         draw_parameter_combinations=False,  # Since we have already drawn the parameters,
@@ -434,7 +433,7 @@ for sfh_name, sfh_params in sfhs.items():
             basis.plot_galaxy(
                 idx=i,
                 out_dir=f"{out_dir}/plots/{name}/",
-                log_stellar_mass=all_param_dict["log_masses"][i],
+                log_stellar_mass=masses[i],
             )
         except ValueError as e:
             print(f"Error plotting galaxy {i}: {e}")

src/synference/grid.py
@@ -4361,7 +4361,7 @@ class CombinedBasis:
             ("log_stellar_masses", self.log_stellar_masses),
             ("combination_weights", self.combination_weights),
         ]:
-            if len(array) != ngal:
+            if not isinstance(array, (int, float)) and len(array) != ngal:
                 raise ValueError(
                     f"""{array_name} length {len(array)} does not match
                     number of galaxies {ngal}."""
@@ -4510,7 +4510,7 @@ class CombinedBasis:
         all_supp_params = []
 
         for pos in range(ngal):
-            redshift = self.redshifts[pos]
+            redshift = self.redshifts[pos] if isinstance(self.redshifts, (np.ndarray, list, tuple)) else self.redshifts
             log_total_mass = self.log_stellar_masses[pos]
             # mass in solar masses
             total_mass = 10**log_total_mass